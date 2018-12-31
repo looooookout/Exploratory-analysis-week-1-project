b5d582529897e454e7847e06c6f9b2be4c0f17d4
looooookout <44877120+looooookout@users.noreply.github.com>
2018-12-31 15:20:29 +0800

    Add files via upload

--- Plot1.R
+++ Plot1.R
@@ -1,12 +1,13 @@
+#Download data file if it did not exits
 dataurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-if (! file.exists("exdata_data_household_power_consumption.zip")){
+if (! file.exists("powerdataset.zip")){
     download.file(dataurl, destfile = "powerdataset.zip", mode ='wb')
 }
 if (!file.exists("./household_power_consumption.txt")){
     unzip("powerdataset.zip")
 }
 
-
+#Read data to R and assign the names for each column
 powerdata <- read.table("household_power_consumption.txt", 
                         na.strings = "?", sep = ";", 
                         skip = (grep("1/2/2007|2/2/2007", 
@@ -14,12 +15,7 @@ powerdata <- read.table("household_power_consumption.txt",
                         nrows = 2880)
 names(powerdata) <- read.table("household_power_consumption.txt",nrows = 1, sep = ";",colClasses = "character")
 
-png("plot1.png", width = 480, height = 480)
-    hist(powerdata$Global_active_power,main = "Global Active Power", 
-         xlab = "Global Active Power(kilowatts)", col = "red")
-dev.off()
-
-#png("plot1.png", width = 480, height = 480)
+#Plot the graph and output
 par(mar=c(4,4,2,2))
 hist(powerdata$Global_active_power, main="Global Active Power",col='red',ylab= "Frequency", xlab="Global Active Power(kilowatts)")
 dev.copy(png, file = "plot1.png", width = 480, height = 480)
